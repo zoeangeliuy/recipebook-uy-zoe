ff36eea88d10fa3302a34cd4f97a1efd85d87649
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-02-09 23:25:35 +0800

    Added recipeslist

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import recipeslist
 
 urlpatterns = [
-
+    path('recipes/list',recipeslist, name='recipes-list')
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
 
-# Create your views here.
+def recipeslist(request):
+    ctx={
+            "recipes": [
+                {
+                    "name": "Recipe 1",
+                    "ingredients": [
+                        {
+                            "name": "tomato",
+                            "quantity": "3pcs"
+                        },
+                        {
+                            "name": "onion",
+                            "quantity": "1pc"
+                        },
+                        {
+                            "name": "pork",
+                            "quantity": "1kg"
+                        },
+                        {
+                            "name": "water",
+                            "quantity": "1L"
+                        },
+                        {
+                            "name": "sinigang mix",
+                            "quantity": "1 packet"
+                        }
+                    ],
+                    "link": "/recipe/1"
+                },
+                {
+                    "name": "Recipe 2",
+                    "ingredients": [
+                        {
+                            "name": "garlic",
+                            "quantity": "1 head"
+                        },
+                        {
+                            "name": "onion",
+                            "quantity": "1pc"
+                        },
+                        {
+                            "name": "vinegar",
+                            "quantity": "1/2cup"
+                        },
+                        {
+                            "name": "water",
+                            "quantity": "1 cup"
+                        },
+                        {
+                            "name": "salt",
+                            "quantity": "1 tablespoon"
+                        },
+                        {
+                            "name": "whole black peppers",
+                            "quantity": "1 tablespoon"
+                        },
+                        {
+                            "name": "pork",
+                            "quantity": "1 kilo"
+                        }
+                    ],
+                    "link": "/recipe/2"
+                }
+            ]
+        }
+    return render(request, 'recipeslist.html',ctx)
+
