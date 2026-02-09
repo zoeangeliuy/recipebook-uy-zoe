215608d993bd065fdaddc680776090728970b06f
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-02-09 23:27:15 +0800

    Added recipe1

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import recipeslist
+from .views import recipeslist, recipe1
 
 urlpatterns = [
-    path('recipes/list',recipeslist, name='recipes-list')
+    path('recipes/list',recipeslist, name='recipes-list'),
+    path('recipe/1',recipe1, name='recipe1'),\
 ]
 
 app_name = "ledger"
--- ledger/views.py
+++ ledger/views.py
@@ -67,3 +67,31 @@ def recipeslist(request):
         }
     return render(request, 'recipeslist.html',ctx)
 
+def recipe1(request):
+    ctx={
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "/recipe/1"
+    }
+    return render(request, 'recipe1.html',ctx)
