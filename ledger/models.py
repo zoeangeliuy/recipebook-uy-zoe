47b26fc79073edf89f98a1e6f8dbaeaf3850e886
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-02-19 18:25:41 +0800

    Added str and absurl functions for Recipe and Ingredient

--- ledger/models.py
+++ ledger/models.py
@@ -1,12 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+   
+    def get_absolute_url(self):
+        return reverse('ingredient_name', args=[str(self.name)])
+        
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+   
+    def get_absolute_url(self):
+        return reverse('recipe_name', args=[str(self.name)])
 
 
 class RecipeIngredient(models.Model):
