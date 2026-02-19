13c8607be6103940f21e2090b548ba6f2f70f542
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-02-19 21:45:19 +0800

    Added RecipeIngredientInLine

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,11 +1,17 @@
 
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient
+
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
 
 class RecipeAdmin(admin.ModelAdmin):
     model=Recipe
 
     search_fields = ('name', )
     list_display = ('name',)
+    inlines = [RecipeIngredientInline,]
 
 admin.site.register(Recipe, RecipeAdmin)
