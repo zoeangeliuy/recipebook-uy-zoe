c932171ec251b1c662676b58b9375136dc3c0409
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-03-02 18:37:54 +0800

    Modified absolute_url

--- ledger/models.py
+++ ledger/models.py
@@ -8,23 +8,17 @@ class Ingredient(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('ingredient_name', args=[str(self.name)])
-
-
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-
+   
     def get_absolute_url(self):
-        return reverse('recipe_name', args=[str(self.name)])
+        return reverse('ledger:recipe', args=[str(self.name)])
 
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
-    ingredient = models.ForeignKey(
-        Ingredient, on_delete=models.CASCADE, related_name="recipe")
-    recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name="ingredients")
+    ingredient=models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipes")
+    recipe=models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
