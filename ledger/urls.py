5c6d34dc8dd1de6cf12c5691fbf87adf02904cc7
VulterZ <zoe.angeli.uy@student.ateneo.edu>
2026-03-02 23:00:44 +0800

    Completed Password Reset

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from .views import RecipesListView, RecipesDetailView
 
 urlpatterns = [
     path('recipes/list', RecipesListView.as_view(), name='recipeslist'),
-    path('recipe/<int:pk>', RecipesDetailView.as_view(), name='recipe'),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('recipe/<int:pk>', RecipesDetailView.as_view(), name='recipe')
 ]
 
 app_name = "ledger"
--- recipebook/urls.py
+++ recipebook/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('ledger.urls', namespace="ledger")),
+    path('', include('ledger.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
